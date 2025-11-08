7ee12ac47f7674a4b3183e8313390832cd4f694b
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 12:49:34 -0500

    feat: creación de iconos y exportación

---
+++ src/rimac/icons/Check.tsx
@@ -0,0 +1,10 @@
+
+import type { IconProps } from './type'
+
+export const Check:React.FC<IconProps> = (props) => {
+    return (
+        <svg width="12" height="10" viewBox="0 0 12 10" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path fillRule="evenodd" clipRule="evenodd" d="M11.4814 0.212971C11.8608 0.534041 11.9081 1.10192 11.587 1.48137L4.98705 9.28137C4.82158 9.47692 4.58063 9.59269 4.32456 9.59968C4.06849 9.60667 3.82158 9.50421 3.64569 9.31798L0.245691 5.71798C-0.0956001 5.35661 -0.0793253 4.787 0.282041 4.44571C0.643408 4.10442 1.21302 4.12069 1.55431 4.48206L4.26316 7.35025L10.213 0.318671C10.534 -0.0607749 11.1019 -0.108098 11.4814 0.212971Z" fill="currentColor"/>
+        </svg>
+    )
+}
--- src/rimac/icons/index.tsx
+++ src/rimac/icons/index.tsx
@@ -1,3 +1,8 @@
 export { Phone } from './Phone';
 export { Separate } from './Separate';
 export { ArrowBack } from './ArrowBack';
+export { Person } from './Person';
+export { PersonPlus } from './PersonPlus';
+export { Check } from './Check';
+export { Home } from './Home';
+export { Clinic } from './Clinic';
