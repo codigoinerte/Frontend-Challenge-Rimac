ba5d3452e71bfc8907b0ab74920d72a01ab7f4ca
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 01:57:46 -0500

    feat: creación de context

---
+++ src/rimac/context/registerContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+
+type User = {
+    name: string;
+    lastName: string;
+    birthDay: Date;
+    documentType: string;
+    document: string;
+    phone: string;
+}
+
+interface registerContextProps {
+    user: User;
+    setUser: (user:User) => void;
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const RegisterContext = createContext({} as registerContextProps);
+
+export const RegisterContextProvider = ({children}:React.PropsWithChildren) => {
+
+    const [user, setUser] = useState<User>({} as User);
+
+    const setParamsUser = (user:User) => setUser(user);
+
+    return (<RegisterContext value={{
+                    user: user,
+                    setUser: setParamsUser
+                }}>
+                {children}
+            </RegisterContext>)
+};
+    
