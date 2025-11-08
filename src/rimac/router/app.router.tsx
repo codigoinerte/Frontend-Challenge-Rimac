787d09d1c47414a8356a88a290db552274578fe3
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 09:24:15 -0500

    feat: integración de ruta plans

--- src/rimac/router/app.router.tsx
+++ src/rimac/router/app.router.tsx
@@ -1,9 +1,12 @@
 import { createBrowserRouter } from "react-router";
-import { Home } from "../pages";
+import { Home, Plans } from "../pages";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
-  },
+  },{
+    path: "/plans",
+    element: <Plans />,
+  }
 ]);
