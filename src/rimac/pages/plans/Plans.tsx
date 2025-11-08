8ffc0d048791cd246de967f7a6c4b02eb484e6ae
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 14:59:11 -0500

    feat: integración de swiper en planes y correcciones de estilos

--- src/rimac/pages/plans/Plans.tsx
+++ src/rimac/pages/plans/Plans.tsx
@@ -1,10 +1,15 @@
+import { Navigation, Pagination } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 import { RegisterContext } from "@/rimac/context/registerContext"
-import { Person, PersonPlus } from "@/rimac/icons"
+import { ArrowBack, Person, PersonPlus } from "@/rimac/icons"
 import { use, useCallback, useEffect, useState } from "react"
 import { CardPeople, Plan } from "./components";
 import type { Plan as PlanType, PlansResponse } from './types/types';
+
+import '@/rimac/styles/swiper.min.css';
+import '@/rimac/styles/pagination.min.css';
+import '@/rimac/styles/customPlans.css';
 
  const cards = [
     {
@@ -56,22 +61,51 @@ export const Plans = () => {
 
   return (
     <>
-      <div className="max-w-[700px] mb-8 text-center">
+      <div className="max-w-[700px] mb-8 text-center mx-auto">
         <h1 className="mb-2 font-lat text-[48px] font-bold leading-12 tracking-[-0.6px] text-[#141938] w-full">{user.name} ¿Para quién deseas cotizar?</h1>
         <p className="font-lato text-[16px] leading-7 tracking-[0.1px] font-normal">Selecciona la opción que se ajuste más a tus necesidades.</p>
       </div>
 
-      <div className="flex flex-row gap-8 mb-5">
+      <div className="flex flex-row gap-8 mb-5 justify-center">
         {
           cards.map(card => (<CardPeople key={card.id} {...card} onClick={() => setSelectedCard(card.id)} selectedCard={selectedCard} />))
         }
       </div>
 
-      <div style={{ display : selectedCard != "" ? 'block' : 'none'}} className='mb-20'>
+      <div style={{ display : selectedCard != "" ? 'block' : 'none'}} className='mb-20 max-w-[985px] w-full'>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={0}
+          navigation={{
+            prevEl: '.custom-prev',
+            nextEl: '.custom-next',
+          }}
+          pagination={{
+            type: "fraction",
+            el: '.custom-pagination',
+          }}
+          modules={[Pagination, Navigation]}
+          className="flex px-10! py-10!"
+          direction='horizontal'
+          onSwiper={(swiper) => console.log('Swiper inicializado', swiper)}
+          onSlideChange={() => console.log('Slide cambiado')}          
+        >
           {
             plans.length > 0 &&
-            plans.map((plan) => <Plan key={plan.name} {...plan} />)
+            plans.map((plan) => <SwiperSlide key={plan.name} className='flex-1 h-auto!'><Plan {...plan} /></SwiperSlide>)
           }
+        </Swiper>
+         {/* Controles personalizados debajo */}
+        <div className="custom-controls">
+          <button className="custom-prev">
+            <ArrowBack width={32} height={32}/>
+          </button>
+          <div className="custom-pagination font-lato"></div>
+          <button className="custom-next rotate-180">
+            <ArrowBack width={32} height={32}/>
+          </button>
+        </div>
+        
       </div>
     </>
   )
