e0c2836a7f7cf9e37fa6204777f20f6bbdabaca6
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 00:28:07 -0500

    feat: implementación de react router

--- src/Rimac.tsx
+++ src/Rimac.tsx
@@ -1,7 +1,15 @@
 import { Home } from "./rimac/pages"
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 export const Rimac = () => {
 
-
-  return (<Home />)
+  return <RouterProvider router={router} />
 }
