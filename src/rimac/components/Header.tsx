bbb3db20b2487992292f1a78a8d29e673fe2238d
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 12:49:03 -0500

    feat: se añadio link en logo header

--- src/rimac/components/Header.tsx
+++ src/rimac/components/Header.tsx
@@ -1,13 +1,16 @@
+import { Link } from 'react-router'
 import { Phone } from '../icons'
 
 export const Header = () => {
     return (
         <header className="max-w-[1120px] w-full px-6 flex flex-row justify-between py-3.5 z-1">
-          <img 
-            src="/images/logo.svg" 
-            alt="Rimac" 
-            title="Rimac" 
-            className="max-w-[65.05px] md:max-w-[79px]"/>
+          <Link to={"/"}>
+            <img 
+              src="/images/logo.svg" 
+              alt="Rimac" 
+              title="Rimac" 
+              className="max-w-[65.05px] md:max-w-[79px]"/>
+          </Link>
           <div className="flex flex-row justify-center items-center gap-4">
             <span className="hidden md:inline-block font-semibold text-[12px]">¡Compra por este medio!</span>
             <a href="tel:014116001" className="cursor-pointer font-bold inline-block text-[16px] md:text-[18px] text-[#03050F] leading-[18px]">
