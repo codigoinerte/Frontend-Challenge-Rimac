19c5d81d87f07d2e4114634fba3e6415f347f1a6
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 14:52:48 -0500

    feat: implementación de step

--- src/rimac/components/Step.tsx
+++ src/rimac/components/Step.tsx
@@ -4,13 +4,20 @@ interface Props{
     number: number;
     label: string;
     isActive: boolean;
+    isDisabled?: boolean;
     className?: string;
     route?: string;
-}
-export const Step:React.FC<Props> = ({isActive, number, label, className, route = "#"}) => {
+}    
+export const Step:React.FC<Props> = ({isActive, isDisabled = false, number, label, className, route = "#"}) => {
     const classNameNumber = isActive ? "bg-[#4F4FFF] text-white " : "text-[#7981B2] border-1 border-[#7981B2] bg-transparent";
     const classNameStep = isActive ? "text-[#141938]" : "text-[#7981B2]";
-    return (<Link to={route} className={`font-bold font-lato flex flex-row gap-4 ${classNameStep} ${className}`}>
+    if(isDisabled)
+        return (<button className={`font-bold font-lato flex flex-row gap-4 ${classNameStep} ${className} cursor-default`} aria-disabled={!isActive} disabled>
+                    <span className={`flex rounded-full w-6 h-6 justify-center items-center text-[12px] font-bold tracking-[0.4px] font-lato! ${classNameNumber}`}>{number}</span>
+                    {label}
+                </button>)
+
+    return (<Link to={route} className={`font-bold font-lato flex flex-row gap-4 ${classNameStep} ${className} ${isActive ? "cursor-pointer" : "cursor-default"}`}>
                 <span className={`flex rounded-full w-6 h-6 justify-center items-center text-[12px] font-bold tracking-[0.4px] font-lato! ${classNameNumber}`}>{number}</span>
                 {label}
             </Link>)
--- src/rimac/components/Stepper.tsx
+++ src/rimac/components/Stepper.tsx
@@ -1,13 +1,27 @@
 import { Separate } from "@/rimac/icons"
 import { Step } from "./Step"
+import { useLocation } from "react-router";
 
 export const Stepper = () => {
+
+    const location = useLocation();
+    
     return (
         <div className="py-4 bg-[#EDEFFC] w-full flex justify-center">
             <div className="max-w-[1120px] w-full px-6 m-0 flex flex-row items-center justify-center">                
-                <Step isActive={true} number={1} label="Planes y coberturas"/>
+                <Step 
+                    route="/planes" 
+                    isActive={location.pathname == "/planes"}                     
+                    number={1} 
+                    label="Planes y coberturas"/>
                 <Separate color="#4F4FFF" className="mx-4"/>
-                <Step isActive={false} number={2} label="Resumen" className="-ms-2"/>                
+                <Step 
+                    route="/planes/resumen" 
+                    isActive={location.pathname == "/planes/resumen"} 
+                    isDisabled={location.pathname == "/planes"}
+                    number={2} 
+                    label="Resumen" 
+                    className="-ms-2"/>
             </div>
         </div>
     )
