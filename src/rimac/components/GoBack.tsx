ce8d63a8cb62a106073a49485962b1ed8746d61c
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 18:19:38 -0500

    fix: corrección de goBack

--- src/rimac/components/GoBack.tsx
+++ src/rimac/components/GoBack.tsx
@@ -1,15 +1,19 @@
+import { useLocation, useNavigate } from 'react-router';
 import { ArrowBack } from '../icons'
-import type { MouseEventHandler } from 'react';
 
 interface Props{
-    className?: string;
-    onClick?: MouseEventHandler<HTMLButtonElement> | undefined;
+    className?: string;    
 }
 
-export const GoBack:React.FC<Props> = ({className ="", onClick}) => {
+export const GoBack:React.FC<Props> = ({className =""}) => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const handleGoBack = () => {
+        navigate((location.pathname === "/planes/resumen") ? "/planes" : "/");        
+    }
     return (
         <div className={`w-full max-w-[928px] mt-10 mb-14 px-6 ${className}`}>
-            <button onClick={onClick} className="font-lato font-bold leading-5 tracking-[0.4px] text-[#4F4FFF] flex flex-row gap-2 cursor-pointer">
+            <button onClick={handleGoBack} className="font-lato font-bold leading-5 tracking-[0.4px] text-[#4F4FFF] flex flex-row gap-2 cursor-pointer">
                 <ArrowBack color="#4F4FFF"/> Volver
             </button>
         </div>
--- src/rimac/layouts/PlanesLayout.tsx
+++ src/rimac/layouts/PlanesLayout.tsx
@@ -1,10 +1,7 @@
-import { Outlet, useNavigate } from "react-router"
+import { Outlet } from "react-router"
 import { GoBack, Header, Stepper } from "../components"
 
 export const PlanesLayout = () => {
-
-    const navigate = useNavigate();
-
 
     return (
         <div className="flex flex-col flex-1 w-full justify-center items-center">
@@ -13,7 +10,7 @@ export const PlanesLayout = () => {
 
             <Stepper />
 
-            <GoBack onClick={() => navigate(-1)}/>
+            <GoBack/>
             
             {/* Body */}
             <main className="flex-1 px-6 md:px-0 w-full">
