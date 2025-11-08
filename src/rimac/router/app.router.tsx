903b5dba11bafaf25e878fcbde1263f505785f82
codigoinerte <fmartinez.bpe@gmail.com>
2025-11-08 10:53:47 -0500

    fix: cambio de estructura de rutas para integrar layout

--- src/rimac/router/app.router.tsx
+++ src/rimac/router/app.router.tsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter } from "react-router";
-import { Home, Plans } from "../pages";
+import { Home, Plans, Resume } from "../pages";
+import { PlanesLayout } from "../layouts/PlanesLayout";
 
 export const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },{
-    path: "/plans",
-    element: <Plans />,
+    path: "/planes/",
+    element: <PlanesLayout/>,
+    children: [
+      {
+        index: true,        
+        element: <Plans />
+      },{
+        path: "resumen",
+        element: <Resume />
+      }
+    ]
   }
 ]);
